315602518d09473ee667bc55abdb1d91bcd31345
johncrash <udsn2@sina.com>
2014-07-21 12:17:20 +0800

    json-c 在win32下编译通过

--- external/json-c/json_config.h
+++ external/json-c/json_config.h
@@ -1,7 +1,12 @@
 #ifdef _WIN32
 #include <stdint.h>
 #include <float.h>
+#include <Windows.h>
 #define INFINITY DBL_MAX
+#define NAN DBL_MAX
 #define HAVE_DECL_NAN
 #define HAVE_DECL_INFINITY
+#define HAVE_DECL_ISINF
+#define isinf(x) (0) //不支持无穷大判断
+#define isnan(x) (0)
 #endif
