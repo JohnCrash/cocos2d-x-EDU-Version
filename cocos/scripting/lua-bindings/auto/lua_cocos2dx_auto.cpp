a2cac270a707ceeffaba747de271bee9014b2871
johncrash <udsn2@sina.com>
2015-04-08 17:53:40 +0800

    fxibug

--- cocos/scripting/lua-bindings/auto/lua_cocos2dx_auto.cpp
+++ cocos/scripting/lua-bindings/auto/lua_cocos2dx_auto.cpp
@@ -95,6 +95,49 @@ int lua_cocos2dx_Ref_retain(lua_State* tolua_S)
 
     return 0;
 }
+int lua_cocos2dx_Ref_autorelease(lua_State* tolua_S)
+{
+    int argc = 0;
+    cocos2d::Ref* cobj = nullptr;
+    bool ok  = true;
+
+#if COCOS2D_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+
+#if COCOS2D_DEBUG >= 1
+    if (!tolua_isusertype(tolua_S,1,"cc.Ref",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    cobj = (cocos2d::Ref*)tolua_tousertype(tolua_S,1,0);
+
+#if COCOS2D_DEBUG >= 1
+    if (!cobj) 
+    {
+        tolua_error(tolua_S,"invalid 'cobj' in function 'lua_cocos2dx_Ref_autorelease'", nullptr);
+        return 0;
+    }
+#endif
+
+    argc = lua_gettop(tolua_S)-1;
+    if (argc == 0) 
+    {
+        if(!ok)
+            return 0;
+        cobj->autorelease();
+        return 0;
+    }
+    CCLOG("%s has wrong number of arguments: %d, was expecting %d \n", "autorelease",argc, 0);
+    return 0;
+
+#if COCOS2D_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_cocos2dx_Ref_autorelease'.",&tolua_err);
+#endif
+
+    return 0;
+}
 int lua_cocos2dx_Ref_getReferenceCount(lua_State* tolua_S)
 {
     int argc = 0;
@@ -153,6 +196,7 @@ int lua_register_cocos2dx_Ref(lua_State* tolua_S)
     tolua_beginmodule(tolua_S,"Ref");
         tolua_function(tolua_S,"release",lua_cocos2dx_Ref_release);
         tolua_function(tolua_S,"retain",lua_cocos2dx_Ref_retain);
+		tolua_function(tolua_S,"autorelease",lua_cocos2dx_Ref_autorelease);
         tolua_function(tolua_S,"getReferenceCount",lua_cocos2dx_Ref_getReferenceCount);
     tolua_endmodule(tolua_S);
     std::string typeName = typeid(cocos2d::Ref).name();
